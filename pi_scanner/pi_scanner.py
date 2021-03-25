e2685e089f30744957ff3008311442e0e61af8c7
Carlos Soto Pérez <carlos348@outlook.com>
2021-03-25 20:41:35 +0000

    Pruebas con RPI. Las pruebas con la RPI exitosas. Escaneo de dispositivos y carga de datos a ThingSpeak.

---
+++ pi_scanner/pi_scanner.py
@@ -0,0 +1,24 @@
+import asyncio
+from bleak import BleakScanner
+import pi_sender
+
+address_list = [
+    "FF:FF:44:46:5F:47",
+    "FF:FF:C0:16:47:9A",
+    "FF:FF:FF:F0:D8:C4"
+]
+
+async def run():
+    devices = await BleakScanner.discover()
+    for d in devices:
+        address = d.address
+        rssi = d.rssi
+        for idx, value in enumerate(address_list):
+            if value == address:
+                print(address, rssi)
+                pi_sender.send_to(idx + 1, int(rssi))
+
+loop = asyncio.get_event_loop()
+while True: 
+    loop.run_until_complete(run())
+    pass
---
+++ pi_scanner/pi_sender.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen
+from time import sleep
+
+def send_to(chanel, data):
+    baseURL = 'http://api.thingspeak.com/update?api_key=REPLACE_KEY&field{}={}'
+    print(baseURL.format(chanel, data))
+    f = urlopen(baseURL.format(chanel, data))
+    f.close()
+    sleep(1)
+    pass
