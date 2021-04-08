f2b0a2ef6ce763935f6d55267533de87ce0c8896
Carlos Soto Pérez <carlos348@outlook.com>
2021-04-08 19:49:11 +0100

    Integración de la función con GCP. Adempas se mejoró la estructura de carpetas.

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,11 +1,39 @@
 import base64
+import datetime
+import firebase_admin
 import json
+import os
+from firebase_admin import credentials, firestore
 
-def hello_pubsub(event, context):
-    deviceId = event['attributes']['deviceId']
-    deviceRegistryId = event['attributes']['deviceRegistryId']
-    message = json.loads(base64.b64decode(event['data']).decode('utf-8'))
-    print("--------- TEST ---------")
-    print(deviceId)
-    print(deviceRegistryId)
-    print(message)
+firestore_credentials_path = os.getenv('FIREBASE_CREDENTIALS_FILE_PATH')
+
+if firestore_credentials_path != None:
+    cred = credentials.Certificate(firestore_credentials_path)
+    default_app = firebase_admin.initialize_app(cred)
+    pass
+db = firestore.client()
+
+def event_beacons(event, context):
+    device_id = event['attributes']['deviceId']
+    registry_id = event['attributes']['deviceRegistryId']
+    messages = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+    now = datetime.datetime.utcnow()
+    update_devicce(device_id, registry_id, now)
+    for msg in messages:
+        update_becon(device_id, msg, now)
+
+def update_becon(device_id, message, now):
+    collection = db.collection('beacons')
+    document = collection.document(message['macAddress'])
+    document.update({
+        'lastRead': now,
+        'rssi': message['rssi']
+    })
+
+def update_devicce(device_id, registry, now):
+    collection = db.collection('dispositivos')
+    document = collection.document('rpi-00')
+    document.update({
+        'lastRead': now
+    })
+
