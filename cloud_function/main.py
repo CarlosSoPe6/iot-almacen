f984b59e70b3b1a8e81219282c6b1013b6c15c7b
Carlos Soto Pérez <carlos348@outlook.com>
2021-04-30 01:15:39 +0100

    piscanner with processing and cloud function working.     Both changes help us to succesfully implement the solution.     For the could function the firebase-admin dep and and key name to match the topic schema.     For piscanner the diference betweeen 2 reads are sent to the MQTT broker.

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,26 +1,28 @@
 import base64
 import datetime
-import firebase_admin
 import json
 import os
-from firebase_admin import credentials, firestore
+from firebase_admin import initialize_app, credentials, firestore
 
 firestore_credentials_path = os.getenv('FIREBASE_CREDENTIALS_FILE_PATH')
 
 if firestore_credentials_path != None:
     cred = credentials.Certificate(firestore_credentials_path)
-    default_app = firebase_admin.initialize_app(cred)
+    default_app = initialize_app(cred)
     pass
+else:
+    initialize_app()
+
 db = firestore.client()
 
-def event_beacons(event, context):
+def hello_pubsub(event, context):
     device_id = event['attributes']['deviceId']
     registry_id = event['attributes']['deviceRegistryId']
-    messages = json.loads(base64.b64decode(event['data']).decode('utf-8'))
+    msg = json.loads(base64.b64decode(event['data']).decode('utf-8'))
     now = datetime.datetime.utcnow()
+    print(msg)
     update_devicce(device_id, registry_id, now)
-    for msg in messages:
-        update_becon(device_id, msg, now)
+    update_becon(device_id, msg, now)
         
 
 def insert_log(beacon, snap_id, now):
@@ -33,7 +35,7 @@ def insert_log(beacon, snap_id, now):
 
 def update_becon(device_id, message, now):
     collection = db.collection('beacons')
-    snap = collection.where('macAddress', '==', message['macAddress']).get()
+    snap = collection.where('macAddress', '==', message['mac_address']).get()
     if len(snap) == 0:
         return
     snap = snap[0]
--- pi_scanner/pi_scanner.py
+++ pi_scanner/pi_scanner.py
@@ -4,12 +4,34 @@ import asyncio
 import json
 from bleak import BleakScanner
 
+# This is not SO functional BTW
+last_read = dict()
+
+def diference(a, b):
+    map = {}
+    for x in b:
+        map[x] = True
+    diff = []
+    for x in a:
+        if x not in map:
+            diff.append(x)
+    return diff
+
 async def run():
     devices = await BleakScanner.discover()
-    records = []
+    addresess = []
     for d in devices:
         address = d.address
         rssi = d.rssi
-        uuid = d.uuid
-        records.append((address, rssi, uuid))
+        uuid = ''
+        if (rssi >= -70):
+            addresess.append(address)
+            if (address in last_read):
+                last_read[address] = rssi
+            else:
+                last_read.update({address: rssi})
+    records = diference(addresess, last_read.keys())
+    for record in records:
+        del last_read[record]
     return records
+
